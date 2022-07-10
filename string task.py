21448e3073ba372b87b3c8781bea0bc6c5c177f6
Hyscient <108424155+Hyscient@users.noreply.github.com>
2022-07-10 16:39:54 +0300

    Add files via upload

---
+++ string task.py
@@ -0,0 +1,17 @@
+Python 3.10.5 (v3.10.5:f377153967, Jun  6 2022, 12:36:10) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+print('There are "married men and women" in my degree programme.')
+There are "married men and women" in my degree programme.
+print("Programming job's the real deal.")
+Programming job's the real deal.
+print(
+    """This string is written on multiple lines,
+    and it displays across white lines"""
+)
+This string is written on multiple lines,
+    and it displays across white lines
+print(
+    "This one-line string was printed out\
+using multiple lines"
+)
+This one-line string was printed outusing multiple lines
